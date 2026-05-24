b59386a35d737ff42f5ccc382694e18fbcfe19d7
Luisandro Bartolomeu <bartolomeuluisandro@gmail.com>
2026-05-24 02:10:53 +0100

    Log inicial

--- src/main/java/com/luisandro/Contactos/ContactosApplication.java
+++ src/main/java/com/luisandro/Contactos/ContactosApplication.java
@@ -1,13 +1,18 @@
 package com.luisandro.Contactos;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContactosApplication {
 
+	private static final Logger log = LogManager.getLogger(ContactosApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ContactosApplication.class, args);
+		log.info("App começou a rodar");
 	}
 
 }
